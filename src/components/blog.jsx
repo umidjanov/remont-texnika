b73ff96d45015412e6350d57188dedd7b78aa806
umidjanov <fayzullohumidjanov@gmail.com>
2024-12-16 16:43:16 +0500

    images

--- src/components/blog.jsx
+++ src/components/blog.jsx
@@ -12,17 +12,17 @@ function Blog() {
             <div className="lg:flex flex-wrap gap-[42px]">
                 <div className="flex flex-col gap-[5px] w-[291px] text-center">
                     <h1 className="text-[#E7742E] text-[60px] font-[500]">95%</h1>
-                    <h2 className="text-[20px]">ремонтных работ выполняются на месте в день обращения</h2>
+                    <h2 className="text-[20px]">Ремонтных работ выполняются на месте в день обращения</h2>
                 </div>
                 <div className="border-[1px] border-gray-400 lg:h-40 md:h-0 h-0"></div>
-                <div className="flex flex-col gap-[5px] w-[291px] text-center">
-                    <h1 className="text-[#E7742E] text-[60px] font-[500]">95%</h1>
-                    <h2 className="text-[20px]">ремонтных работ выполняются на месте в день обращения</h2>
+                <div className="flex flex-col gap-[23px] w-[291px] text-center items-center">
+                    <img className="w-[100px]" src="public/purse.png" alt="kashelok" />
+                    <h2 className="text-[20px]">Демократичные цены, отсутствие скрытых услуг и доплат</h2>
                 </div>
                 <div className="border-[1px] border-gray-400 lg:h-40 md:h-0 h-0"></div>
-                <div className="flex flex-col gap-[5px] w-[291px] text-center">
-                    <h1 className="text-[#E7742E] text-[60px] font-[500]">95%</h1>
-                    <h2 className="text-[20px]">ремонтных работ выполняются на месте в день обращения</h2>
+                <div className="flex flex-col gap-[19px] w-[291px] text-center items-center">
+                    <img className="w-[110px]" src="public/sheet.png" alt="malumot" />
+                    <h2 className="text-[20px]">Гарантия на ремонтные работы и комплектующие один год</h2>
                 </div>
             </div>
             <div className="flex gap-[25px] items-center">
@@ -33,19 +33,19 @@ function Blog() {
                 <div className="lg:flex hidden w-[300px] border-[1px] border-gray-400 h-0"></div>
             </div>
             <div className="lg:flex flex-wrap gap-[42px] md:text-center">
-                <div className="flex flex-col gap-[5px] w-[291px] text-center">
-                    <h1 className="text-[#E7742E] text-[60px] font-[500]">95%</h1>
-                    <h2 className="text-[20px]">ремонтных работ выполняются на месте в день обращения</h2>
+                <div className="flex flex-col gap-[30px] items-center w-[291px] text-center">
+                    <img className="w-[110px]" src="public/pro.png" alt="" />
+                    <h2 className="text-[18px]">Все мастера – дипломированные специалисты с опытом работы более 8 лет</h2>
                 </div>
                 <div className="border-[1px] border-gray-400 lg:h-40 md:h-0 h-0"></div>
-                <div className="flex flex-col gap-[5px] w-[291px] text-center">
-                    <h1 className="text-[#E7742E] text-[60px] font-[500]">95%</h1>
-                    <h2 className="text-[20px]">ремонтных работ выполняются на месте в день обращения</h2>
+                <div className="flex flex-col gap-[30px] items-center w-[291px] text-center">
+                    <img className="w-[110px]" src="public/wheels.png" alt="" />
+                    <h2 className="text-[18px]">Современное профессиональное оборудование для диагностики и ремонта</h2>
                 </div>
                 <div className="border-[1px] border-gray-400 lg:h-40 md:h-0 h-0"></div>
-                <div className="flex flex-col gap-[5px] w-[291px] text-center">
-                    <h1 className="text-[#E7742E] text-[60px] font-[500]">95%</h1>
-                    <h2 className="text-[20px]">ремонтных работ выполняются на месте в день обращения</h2>
+                <div className="flex flex-col gap-[29px] items-center w-[291px] text-center">
+                    <img className="w-[110px]" src="public/notebook.png" alt="notebook" />
+                    <h2 className="text-[20px]">При необходимости проведем ремонт вечером или в выходной день</h2>
                 </div>
             </div>
             <div className="flex gap-[25px] items-center">
@@ -56,19 +56,19 @@ function Blog() {
                 <div className="lg:flex hidden w-[300px] border-[1px] border-gray-400 h-0"></div>
             </div>
             <div className="lg:flex flex-wrap gap-[42px]">
-                <div className="flex flex-col gap-[5px] w-[291px] text-center">
-                    <h1 className="text-[#E7742E] text-[60px] font-[500]">95%</h1>
-                    <h2 className="text-[20px]">ремонтных работ выполняются на месте в день обращения</h2>
+                <div className="flex flex-col gap-[30px] items-center w-[291px] text-center">
+                    <img className="w-[110px]" src="public/box.png" alt="box" />
+                    <h2 className="text-[20px]">Все запчасти только от официальных дистрибьюторов</h2>
                 </div>
                 <div className="border-[1px] border-gray-400 lg:h-40 md:h-0 h-0"></div>
-                <div className="flex flex-col gap-[5px] w-[291px] text-center">
-                    <h1 className="text-[#E7742E] text-[60px] font-[500]">95%</h1>
-                    <h2 className="text-[20px]">ремонтных работ выполняются на месте в день обращения</h2>
+                <div className="flex flex-col gap-[30px] items-center w-[291px] text-center">
+                    <img className="w-[110px]" src="public/windows.png" alt="" />
+                    <h2 className="text-[20px]">Лицензионные ключи на программное обеспечение по оптовой цене </h2>
                 </div>
                 <div className="border-[1px] border-gray-400 lg:h-40 md:h-0 h-0"></div>
-                <div className="flex flex-col gap-[5px] w-[291px] text-center">
-                    <h1 className="text-[#E7742E] text-[60px] font-[500]">95%</h1>
-                    <h2 className="text-[20px]">ремонтных работ выполняются на месте в день обращения</h2>
+                <div className="flex flex-col gap-[30px] items-center w-[291px] text-center">
+                    <img className="w-[110px]" src="public/people.png" alt="peopple" />
+                    <h2 className="text-[20px]">9 из 10 обратившихся рекомендуют нас друзьям и знакомым </h2>
                 </div>
             </div>
             <div className="flex gap-[25px] items-center">
