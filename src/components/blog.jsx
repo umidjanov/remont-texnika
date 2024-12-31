02178cdc863a92b07828be17cade6281ec0b4e88
umidjanov <fayzullohumidjanov@gmail.com>
2024-12-31 19:41:48 +0500

    blog

--- src/components/blog.jsx
+++ src/components/blog.jsx
@@ -1,85 +1,106 @@
 function Blog() {
-    return (
-        <div className="p-[30px] b-[30px] flex flex-col items-center gap-[20px]">
-            <h1 className="lg:text-[38px] md:text-[28px] text-[28px] lg:w-[900px] w-[300px] text-center text-[#E7742E]">ПОЧЕМУ СТОИТ ОБРАТИТЬСЯ К НАМ</h1>
-            <div className="flex gap-[25px] items-center text-center">
-                <div className="lg:flex hidden w-[300px] border-[1px] border-gray-400 h-0"></div>
-                <div className="lg:flex hidden w-[25px] h-[25px] rounded-full bg-[#E7742E]"></div>
-                <div className="lg:w-[300px] md:w-[400px] w-[250px] border-[1px] border-gray-400 h-0"></div>
-                <div className="lg:flex hidden w-[25px] h-[25px] rounded-full bg-[#E7742E]"></div>
-                <div className="lg:flex hidden w-[300px] border-[1px] border-gray-400 h-0"></div>
-            </div>
-            <div className="lg:flex flex-wrap gap-[42px]">
-                <div className="flex flex-col gap-[5px] w-[291px] text-center">
-                    <h1 className="text-[#E7742E] text-[60px] font-[500]">95%</h1>
-                    <h2 className="text-[20px]">Ремонтных работ выполняются на месте в день обращения</h2>
-                </div>
-                <div className="border-[1px] border-gray-400 lg:h-40 md:h-0 h-0"></div>
-                <div className="flex flex-col gap-[23px] w-[291px] text-center items-center">
-                    <img className="w-[100px]" src="public/purse.png" alt="kashelok" />
-                    <h2 className="text-[20px]">Демократичные цены, отсутствие скрытых услуг и доплат</h2>
-                </div>
-                <div className="border-[1px] border-gray-400 lg:h-40 md:h-0 h-0"></div>
-                <div className="flex flex-col gap-[19px] w-[291px] text-center items-center">
-                    <img className="w-[110px]" src="public/sheet.png" alt="malumot" />
-                    <h2 className="text-[20px]">Гарантия на ремонтные работы и комплектующие один год</h2>
-                </div>
-            </div>
-            <div className="flex gap-[25px] items-center">
-                <div className="lg:flex hidden w-[300px] border-[1px] border-gray-400 h-0"></div>
-                <div className="lg:flex hidden w-[25px] h-[25px] rounded-full bg-[#E7742E]"></div>
-                <div className="w-[300px] border-[1px] border-gray-400 h-0"></div>
-                <div className="lg:flex hidden w-[25px] h-[25px] rounded-full bg-[#E7742E]"></div>
-                <div className="lg:flex hidden w-[300px] border-[1px] border-gray-400 h-0"></div>
-            </div>
-            <div className="lg:flex flex-wrap gap-[42px] md:text-center">
-                <div className="flex flex-col gap-[30px] items-center w-[291px] text-center">
-                    <img className="w-[110px]" src="public/pro.png" alt="" />
-                    <h2 className="text-[18px]">Все мастера – дипломированные специалисты с опытом работы более 8 лет</h2>
-                </div>
-                <div className="border-[1px] border-gray-400 lg:h-40 md:h-0 h-0"></div>
-                <div className="flex flex-col gap-[30px] items-center w-[291px] text-center">
-                    <img className="w-[110px]" src="public/wheels.png" alt="" />
-                    <h2 className="text-[18px]">Современное профессиональное оборудование для диагностики и ремонта</h2>
-                </div>
-                <div className="border-[1px] border-gray-400 lg:h-40 md:h-0 h-0"></div>
-                <div className="flex flex-col gap-[29px] items-center w-[291px] text-center">
-                    <img className="w-[110px]" src="public/notebook.png" alt="notebook" />
-                    <h2 className="text-[20px]">При необходимости проведем ремонт вечером или в выходной день</h2>
-                </div>
-            </div>
-            <div className="flex gap-[25px] items-center">
-                <div className="lg:flex hidden w-[300px] border-[1px] border-gray-400 h-0"></div>
-                <div className="lg:flex hidden w-[25px] h-[25px] rounded-full bg-[#E7742E]"></div>
-                <div className="w-[300px] border-[1px] border-gray-400 h-0"></div>
-                <div className="lg:flex hidden w-[25px] h-[25px] rounded-full bg-[#E7742E]"></div>
-                <div className="lg:flex hidden w-[300px] border-[1px] border-gray-400 h-0"></div>
-            </div>
-            <div className="lg:flex flex-wrap gap-[42px]">
-                <div className="flex flex-col gap-[30px] items-center w-[291px] text-center">
-                    <img className="w-[110px]" src="public/box.png" alt="box" />
-                    <h2 className="text-[20px]">Все запчасти только от официальных дистрибьюторов</h2>
-                </div>
-                <div className="border-[1px] border-gray-400 lg:h-40 md:h-0 h-0"></div>
-                <div className="flex flex-col gap-[30px] items-center w-[291px] text-center">
-                    <img className="w-[110px]" src="public/windows.png" alt="" />
-                    <h2 className="text-[20px]">Лицензионные ключи на программное обеспечение по оптовой цене </h2>
-                </div>
-                <div className="border-[1px] border-gray-400 lg:h-40 md:h-0 h-0"></div>
-                <div className="flex flex-col gap-[30px] items-center w-[291px] text-center">
-                    <img className="w-[110px]" src="public/people.png" alt="peopple" />
-                    <h2 className="text-[20px]">9 из 10 обратившихся рекомендуют нас друзьям и знакомым </h2>
-                </div>
-            </div>
-            <div className="flex gap-[25px] items-center">
-                <div className="lg:flex hidden w-[300px] border-[1px] border-gray-400 h-0"></div>
-                <div className="lg:flex hidden w-[25px] h-[25px] rounded-full bg-[#E7742E]"></div>
-                <div className="lg:flex md:flex hidden w-[300px] border-[1px] border-gray-400 h-0"></div>
-                <div className="lg:flex hidden w-[25px] h-[25px] rounded-full bg-[#E7742E]"></div>
-                <div className="lg:flex hidden w-[300px] border-[1px] border-gray-400 h-0"></div>
-            </div>
+  return (
+    <div className="p-[30px] b-[30px] flex flex-col items-center gap-[20px]">
+      <h1 className="lg:text-[38px] md:text-[28px] text-[28px] lg:w-[900px] w-[300px] text-center text-[#E7742E]">
+        ПОЧЕМУ СТОИТ ОБРАТИТЬСЯ К НАМ
+      </h1>
+      <div className="flex gap-[25px] items-center text-center">
+        <div className="lg:flex hidden w-[300px] border-[1px] border-gray-400 h-0"></div>
+        <div className="lg:flex hidden w-[25px] h-[25px] rounded-full bg-[#E7742E]"></div>
+        <div className="lg:w-[300px] md:w-[400px] w-[250px] border-[1px] border-gray-400 h-0"></div>
+        <div className="lg:flex hidden w-[25px] h-[25px] rounded-full bg-[#E7742E]"></div>
+        <div className="lg:flex hidden w-[300px] border-[1px] border-gray-400 h-0"></div>
+      </div>
+      <div className="lg:flex flex-wrap gap-[42px]">
+        <div className="flex flex-col gap-[5px] w-[291px] text-center">
+          <h1 className="text-[#E7742E] text-[60px] font-[500]">95%</h1>
+          <h2 className="text-[20px]">
+            Ремонтных работ выполняются на месте в день обращения
+          </h2>
         </div>
-    )
+        <div className="border-[1px] border-gray-400 lg:h-40 md:h-0 h-0"></div>
+        <div className="flex flex-col gap-[23px] w-[291px] text-center items-center">
+          <img className="w-[100px]" src="public/purse.png" alt="kashelok" />
+          <h2 className="text-[20px]">
+            Демократичные цены, отсутствие скрытых услуг и доплат
+          </h2>
+        </div>
+        <div className="border-[1px] border-gray-400 lg:h-40 md:h-0 h-0"></div>
+        <div className="flex flex-col gap-[19px] w-[291px] text-center items-center">
+          <img className="w-[110px]" src="public/sheet.png" alt="malumot" />
+          <h2 className="text-[20px]">
+            Гарантия на ремонтные работы и комплектующие один год
+          </h2>
+        </div>
+      </div>
+      <div className="flex gap-[25px] items-center">
+        <div className="lg:flex hidden w-[300px] border-[1px] border-gray-400 h-0"></div>
+        <div className="lg:flex hidden w-[25px] h-[25px] rounded-full bg-[#E7742E]"></div>
+        <div className="w-[300px] border-[1px] border-gray-400 h-0"></div>
+        <div className="lg:flex hidden w-[25px] h-[25px] rounded-full bg-[#E7742E]"></div>
+        <div className="lg:flex hidden w-[300px] border-[1px] border-gray-400 h-0"></div>
+      </div>
+      <div className="lg:flex flex-wrap gap-[42px] md:text-center">
+        <div className="flex flex-col gap-[30px] items-center w-[291px] text-center">
+          <img className="w-[110px]" src="public/pro.png" alt="" />
+          <h2 className="text-[18px]">
+            Все мастера – дипломированные специалисты с опытом работы более 8
+            лет
+          </h2>
+        </div>
+        <div className="border-[1px] border-gray-400 lg:h-40 md:h-0 h-0"></div>
+        <div className="flex flex-col gap-[30px] items-center w-[291px] text-center">
+          <img className="w-[110px]" src="public/wheels.png" alt="" />
+          <h2 className="text-[18px]">
+            Современное профессиональное оборудование для диагностики и ремонта
+          </h2>
+        </div>
+        <div className="border-[1px] border-gray-400 lg:h-40 md:h-0 h-0"></div>
+        <div className="flex flex-col gap-[29px] items-center w-[291px] text-center">
+          <img className="w-[110px]" src="public/notebook.png" alt="notebook" />
+          <h2 className="text-[20px]">
+            При необходимости проведем ремонт вечером или в выходной день
+          </h2>
+        </div>
+      </div>
+      <div className="flex gap-[25px] items-center">
+        <div className="lg:flex hidden w-[300px] border-[1px] border-gray-400 h-0"></div>
+        <div className="lg:flex hidden w-[25px] h-[25px] rounded-full bg-[#E7742E]"></div>
+        <div className="w-[300px] border-[1px] border-gray-400 h-0"></div>
+        <div className="lg:flex hidden w-[25px] h-[25px] rounded-full bg-[#E7742E]"></div>
+        <div className="lg:flex hidden w-[300px] border-[1px] border-gray-400 h-0"></div>
+      </div>
+      <div className="lg:flex flex-wrap gap-[42px]">
+        <div className="flex flex-col gap-[30px] items-center w-[291px] text-center">
+          <img className="w-[110px]" src="public/box.png" alt="box" />
+          <h2 className="text-[20px]">
+            Все запчасти только от официальных дистрибьюторов
+          </h2>
+        </div>
+        <div className="border-[1px] border-gray-400 lg:h-40 md:h-0 h-0"></div>
+        <div className="flex flex-col gap-[30px] items-center w-[291px] text-center">
+          <img className="w-[110px]" src="public/windows.png" alt="" />
+          <h2 className="text-[20px]">
+            Лицензионные ключи на программное обеспечение по оптовой цене{" "}
+          </h2>
+        </div>
+        <div className="border-[1px] border-gray-400 lg:h-40 md:h-0 h-0"></div>
+        <div className="flex flex-col gap-[30px] items-center w-[291px] text-center">
+          <img className="w-[110px]" src="public/people.png" alt="peopple" />
+          <h2 className="text-[20px]">
+            9 из 10 обратившихся рекомендуют нас друзьям и знакомым{" "}
+          </h2>
+        </div>
+      </div>
+      <div className="flex gap-[25px] items-center">
+        <div className="lg:flex hidden w-[300px] border-[1px] border-gray-400 h-0"></div>
+        <div className="lg:flex hidden w-[25px] h-[25px] rounded-full bg-[#E7742E]"></div>
+        <div className="lg:flex md:flex hidden w-[300px] border-[1px] border-gray-400 h-0"></div>
+        <div className="lg:flex hidden w-[25px] h-[25px] rounded-full bg-[#E7742E]"></div>
+        <div className="lg:flex hidden w-[300px] border-[1px] border-gray-400 h-0"></div>
+      </div>
+    </div>
+  );
 }
 
 export default Blog;
