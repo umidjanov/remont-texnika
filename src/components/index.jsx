b2b4bb0607442cd5ec2de09ac4ff4eeca9e57a32
umidjanov <fayzullohumidjanov@gmail.com>
2024-12-31 17:32:55 +0500

    index

--- src/components/index.jsx
+++ src/components/index.jsx
@@ -1,19 +1,19 @@
 function Jaloba() {
   return (
     <div className="bg-[#0B1427] p-[50px] flex items-center flex-col text-white gap-[30px]">
-      <div className="lg:flex hidden border-[1px] border-[#E7742E] lg:w-[1000px] w-[300px]"></div>
+      <div className="lg:flex border-[1px] border-[#E7742E] lg:w-[1000px] w-[300px] md:w-[450px]"></div>
       <div className="lg:flex gap-[40px] lg:w-[1000px] lg:items-center">
         <img
           className="lg:flex hidden"
           src="public/Group 77.png"
           alt=""
         />
-        <div className="lg:hidden flex items-center">
-          <div className="lg:hidden flex border-[1px] border-[#E7742E] lg:w-[1000px] w-[160px] h-0"></div>
+        {/* <div className="lg:hidden flex items-center">
+          <div className="lg:hidden flex border-[1px] border-[#E7742E] lg:w-[1000px] w-[160px] h-0 md:w-[300px]"></div>
           <img src="public/Icon102x96_desktop/Group 77.png" alt="" />
           <div className="lg:hidden flex border-[1px] border-[#E7742E] lg:w-[1000px] w-[160px] h-0"></div>
-        </div>
-        <h1 className="lg:text-[22px] lg:text-start text-center lg:w-[1000px] w-[300px] text-[20px]">
+        </div> */}
+        <h1 className="lg:text-[22px] lg:text-start text-center lg:w-[1000px] w-[300px] text-[20px] md:text-[17px] md:w-[500px]">
           Жаль, нельзя заранее предугадать будущую поломку в компьютере. Она
           становится заметной, лишь когда тот окончательно перестает работать.
           Важно вовремя обратиться в сервисный центр для предоставления
@@ -21,7 +21,7 @@ function Jaloba() {
           от лишних затрат и стрессовых ситуаций.
         </h1>
       </div>
-      <div className="border-[1px] border-[#E7742E] lg:w-[1000px] w-[300px]"></div>
+      <div className="border-[1px] border-[#E7742E] lg:w-[1000px] w-[300px] md:w-[450px]"></div>
     </div>
   );
 }
