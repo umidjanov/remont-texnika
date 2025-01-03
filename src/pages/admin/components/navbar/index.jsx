ed78496d7194cc84924b5951b59b1c9e9b17c68e
umidjanov <fayzullohumidjanov@gmail.com>
2025-01-03 16:19:24 +0500

    src

--- src/pages/admin/components/navbar/index.jsx
+++ src/pages/admin/components/navbar/index.jsx
@@ -7,6 +7,7 @@ import {
   IconButton,
 } from "@material-tailwind/react";
 import { Link } from "react-router-dom";
+import News from "../../pages/news";
 
 export default function NavbarDefault() {
   const [openNav, setOpenNav] = React.useState(false);
@@ -39,10 +40,8 @@ export default function NavbarDefault() {
           />
         </svg>
 
-        <Link to={"/news"}>
-          <a href="#" className="flex items-center">
-            News
-          </a>
+        <Link path={"/news"} element={<News />}>
+          News
         </Link>
       </Typography>
       <Typography
@@ -121,73 +120,76 @@ export default function NavbarDefault() {
   );
 
   return (
-    <Navbar
-      style={{ background: "#0B1427" }}
-      className="mx-start max-w-[250px] border-[#0B1427] backdrop-blur-none rounded-none px-4 lg:px-8 lg:py-4 h-[100vh] bg-[#0B1427]"
-    >
-      <div className="container flex flex-col gap-[20px] text-center">
-        <Link to={"/"}>
-          <Typography
-            as="a"
-            href="#"
-            className=" cursor-pointer w-[201.5px] font-bold text-white"
+    <>
+      <NavbarDefault />
+      <Navbar
+        style={{ background: "#0B1427" }}
+        className="mx-start max-w-[250px] border-[#0B1427] backdrop-blur-none rounded-none px-4 lg:px-8 lg:py-4 h-[100vh] bg-[#0B1427]"
+      >
+        <div className="container flex flex-col gap-[20px] text-center">
+          <Link to={"/"}>
+            <Typography
+              as="a"
+              href="#"
+              className=" cursor-pointer w-[201.5px] font-bold text-white"
+            >
+              РЕМОНТ В ТЕХНИКЕ
+            </Typography>
+          </Link>
+          <div className="hidden lg:block">{navList}</div>
+          <div className="flex flex-col gap-[20px] items-center gap-x-1"></div>
+          <IconButton
+            variant="text"
+            className="ml-auto h-6 w-6 text-inherit hover:bg-transparent  focus:bg-transparent active:bg-transparent lg:hidden"
+            ripple={false}
+            onClick={() => setOpenNav(!openNav)}
           >
-            РЕМОНТ В ТЕХНИКЕ
-          </Typography>
-        </Link>
-        <div className="hidden lg:block">{navList}</div>
-        <div className="flex flex-col gap-[20px] items-center gap-x-1"></div>
-        <IconButton
-          variant="text"
-          className="ml-auto h-6 w-6 text-inherit hover:bg-transparent  focus:bg-transparent active:bg-transparent lg:hidden"
-          ripple={false}
-          onClick={() => setOpenNav(!openNav)}
-        >
-          {openNav ? (
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              fill="none"
-              className="h-6 w-6"
-              viewBox="0 0 24 24"
-              stroke="currentColor"
-              strokeWidth={2}
-            >
-              <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                d="M6 18L18 6M6 6l12 12"
-              />
-            </svg>
-          ) : (
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              className="h-6 w-6"
-              fill="none"
-              stroke="currentColor"
-              strokeWidth={2}
-            >
-              <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                d="M4 6h16M4 12h16M4 18h16"
-              />
-            </svg>
-          )}
-        </IconButton>
-      </div>
-      <MobileNav open={openNav}>
-        <div className="container mx-auto">
-          {navList}
-          <div className="flex items-center gap-x-1">
-            <Button fullWidth variant="text" size="sm" className="">
-              <span>Log In</span>
-            </Button>
-            <Button fullWidth variant="gradient" size="sm" className="">
-              <span>Sign in</span>
-            </Button>
-          </div>
+            {openNav ? (
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                className="h-6 w-6"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
+                strokeWidth={2}
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  d="M6 18L18 6M6 6l12 12"
+                />
+              </svg>
+            ) : (
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                className="h-6 w-6"
+                fill="none"
+                stroke="currentColor"
+                strokeWidth={2}
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  d="M4 6h16M4 12h16M4 18h16"
+                />
+              </svg>
+            )}
+          </IconButton>
         </div>
-      </MobileNav>
-    </Navbar>
+        <MobileNav open={openNav}>
+          <div className="container mx-auto">
+            {navList}
+            <div className="flex items-center gap-x-1">
+              <Button fullWidth variant="text" size="sm" className="">
+                <span>Log In</span>
+              </Button>
+              <Button fullWidth variant="gradient" size="sm" className="">
+                <span>Sign in</span>
+              </Button>
+            </div>
+          </div>
+        </MobileNav>
+      </Navbar>
+    </>
   );
 }
--- src/pages/admin/pages/create/index.jsx
+++ src/pages/admin/pages/create/index.jsx
@@ -5,7 +5,6 @@ import { Input } from "@material-tailwind/react";
 import NavbarDefault from "../../components/navbar";
 
 export default function Create() {
-  <NavbarDefault />;
   const [name, setName] = useState("");
   const [createAt, setCreateAt] = useState("");
   const [avatar, setAvatar] = useState("");
@@ -19,48 +18,51 @@ export default function Create() {
   };
 
   return (
-    <div className="bg-blue-gray-100 ml-[-120px] w-[100%]">
-      <div className="pl-[40px] bg-[#0B1427] text-white uppercase">
-        <h1 className="pt-6 border-l ml-[-20px] p-3 w-full font-bold">
-          this is Create page
-        </h1>
-      </div>
+    <div className="flex">
+      <NavbarDefault />
+      <div className="bg-blue-gray-100 ml-[-120px] w-[100%]">
+        <div className="pl-[40px] bg-[#0B1427] text-white uppercase">
+          <h1 className="pt-6 border-l ml-[-20px] p-3 w-full font-bold">
+            this is Create page
+          </h1>
+        </div>
 
-      <div className="bg-white w-[1120px] mt-[60px] h-[380px] rounded-2xl ml-[90px] m-4">
-        <div className=" pl-[200px] w-[1100px] pt-[80px]">
-          {/* <h1 className="text-[#E7742E] mt-[-40px] uppercase ml-[10px] font-bold">
+        <div className="bg-white w-[1120px] mt-[60px] h-[380px] rounded-2xl ml-[90px] m-4">
+          <div className=" pl-[200px] w-[1100px] pt-[80px]">
+            {/* <h1 className="text-[#E7742E] mt-[-40px] uppercase ml-[10px] font-bold">
             this is Create page
           </h1>
           <hr className="font-bold w-[700px] border-black" /> */}
-          <form
-            onSubmit={(e) => create(e)}
-            className="flex mt-6 flex-col mr-[200px] gap-8 justify-center items-center"
-          >
-            <Input
-              className="h-[49px]"
-              value={name}
-              onChange={(e) => setName(e.target.value)}
-              type="text"
-              label="title"
-            />
-            <Input
-              className="h-[49px]"
-              value={createAt}
-              onChange={(e) => setCreateAt(e.target.value)}
-              type="text"
-              label="desc"
-            />
-            <Input
-              className="h-[49px]"
-              value={avatar}
-              onChange={(e) => setAvatar(e.target.value)}
-              type="text"
-              label="img"
-            />
-            <button className="bg-[#E7742E] rounded-lg px-[19px] text-black border-[#E7742E]">
-              create
-            </button>
-          </form>
+            <form
+              onSubmit={(e) => create(e)}
+              className="flex mt-6 flex-col mr-[200px] gap-8 justify-center items-center"
+            >
+              <Input
+                className="h-[49px]"
+                value={name}
+                onChange={(e) => setName(e.target.value)}
+                type="text"
+                label="title"
+              />
+              <Input
+                className="h-[49px]"
+                value={createAt}
+                onChange={(e) => setCreateAt(e.target.value)}
+                type="text"
+                label="desc"
+              />
+              <Input
+                className="h-[49px]"
+                value={avatar}
+                onChange={(e) => setAvatar(e.target.value)}
+                type="text"
+                label="img"
+              />
+              <button className="bg-[#E7742E] rounded-lg px-[19px] text-black border-[#E7742E]">
+                create
+              </button>
+            </form>
+          </div>
         </div>
       </div>
     </div>
