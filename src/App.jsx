86c8b14a874f44447ee1b57ba9b0b11c7765f3eb
umidjanov <fayzullohumidjanov@gmail.com>
2025-01-03 15:49:53 +0500

    src

--- src/App.jsx
+++ src/App.jsx
@@ -5,12 +5,14 @@ import AdminHome from "./pages/admin/pages/home";
 import Create from "./pages/admin/pages/create";
 import News from "./pages/admin/pages/news";
 import Update from "./pages/admin/pages/update";
+import NavbarDefault from "./pages/admin/components/navbar";
 
 export default function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/navbar" element={<NavbarDefault />} />
         <Route path="/admin" element={<AdminHome />} />
         <Route path="/admin/create" element={<Create />} />
         <Route path="/admin/news" element={<News />} />
