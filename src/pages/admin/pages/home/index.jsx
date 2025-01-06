b061410abb639c13f43b2b5f798fb90c5f75a52d
umidjanov <fayzullohumidjanov@gmail.com>
2025-01-06 22:47:11 +0500

    src

--- src/pages/admin/pages/home/index.jsx
+++ src/pages/admin/pages/home/index.jsx
@@ -1,10 +1,16 @@
 import NavbarDefault from "../../components/navbar";
 
 export default function Home() {
-    return(
-        <div className="flex">
-            <NavbarDefault />
-            <h1 className="m-[100px]">this is Home page</h1>
-        </div>
-    )
+  return (
+    <div className="flex">
+      <NavbarDefault />
+      <h1 className="m-[100px]">this is Home page</h1>
+      <Stack spacing={2}>
+        <Pagination count={10} />
+        <Pagination count={10} color="primary" />
+        <Pagination count={10} color="secondary" />
+        <Pagination count={10} disabled />
+      </Stack>
+    </div>
+  );
 }
